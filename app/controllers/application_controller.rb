38fb6d0f21d54bf7593582f99f163f77b9300d2e
Bartosz Borowiecki <bartosz.borowiecki@gmail.com>
2014-06-10 08:43:59 +0200

    Add user following

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,22 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
-  include SessionsHelper
+	before_action :signed_in_user, only: [:index, :edit, :update, :destroy, :following, :followers]
+
+	def following
+	    @title = "Following"
+	    @user = User.find(params[:id])
+	    @users = @user.followed_users.paginate(page: params[:page])
+	    render 'show_follow'
+	end
+
+	def followers
+	    @title = "Followers"
+	    @user = User.find(params[:id])
+	    @users = @user.followers.paginate(page: params[:page])
+	    render 'show_follow'
+	end
+
+  	protect_from_forgery with: :exception
+  	include SessionsHelper
 end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,16 @@
 class StaticPagesController < ApplicationController
+  #def home
+   # @micropost = current_user.microposts.build if signed_in?
+    #if current_user != nil
+     # @feed_items = current_user.feed.paginate(page: params[:page])
+    #end
+  #end
+
   def home
-    @micropost = current_user.microposts.build if signed_in?
-    @feed_items = current_user.feed.paginate(page: params[:page])
+    if signed_in?
+      @micropost  = current_user.microposts.build
+      @feed_items = current_user.feed.paginate(page: params[:page])
+    end
   end
 
   def help
