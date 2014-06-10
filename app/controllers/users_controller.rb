a0b4dd229a44ae15e5d6f2993cfe68123f2fc210
Bartosz Borowiecki <bartosz.borowiecki@gmail.com>
2014-06-10 09:05:46 +0200

    Added welcome mail

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,6 +25,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      UserMailer.welcome_email(@user).deliver
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
